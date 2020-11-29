0faeff5302b4f8580b55eed4f00114fc402ccb18
Barnabas27 <bnyambura032@gmail.com>
2020-11-29 13:23:06 +0300

    initializing Bootsrap class

--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
 from flask import render_template
 from app import app
+from request import get_news
 
 @app.route('/')
 def index():
     
-    title = 'Welcome to your A.M to the P.M News.'
-    return render_template('index.html', title = title)
+    popular_news = get_news('popular')
+    print(popular_news)
+    title = 'Home - Here is news for you'
+    return render_template('index.html',title = title, popular = popular_news)
+    
 
-@app.route('/news/<news_id>')
-def news(news_id):
-    return render_template('news.html',id = news_id)
+
+# @app.route('/news/<news_id>')
+# def news(news_id):
+#     return render_template('news.html',id = news_id)
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# import pdb; pdb.set_trace()
 from app import app
 
 if __name__ == '__main__':
